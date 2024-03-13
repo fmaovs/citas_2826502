7fd4bb048153ea6bf05b1ef1daa459bca48e7186
unknown <SlenderCrak1@gmail.com>
2024-03-12 23:12:01 -0500

    formularios

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from . import app
+from . import app,db
 from .models import Medico, Paciente, Consultorio, Cita
 from flask import render_template, request
 
@@ -35,6 +35,18 @@ def get_paciente_by_id(id):
     paciente = Paciente.query.get(id)
     return render_template("paciente.html",
                            pac = paciente )
+    
+@app.route("/consultorios/<int:id>")
+def get_consultorio_by_id(id):
+    consultorio = Consultorio.query.get(id)
+    return render_template("consultorio.html",
+                           con = consultorio)
+    
+@app.route("/citas/<int:id>")
+def get_cita_by_id(id):
+    cita = Cita.query.get(id)
+    return render_template("cita.html",
+                           cit = cita)
 
 #crear ruta para crear nuevo medico
 @app.route("/medicos/create" , methods = [ "GET" , "POST"] )
@@ -67,3 +79,96 @@ def create_medico():
         db.session.add(new_medico)
         db.session.commit()
         return "medico registrado"
+
+#crear ruta para crear nuevo paciente
+@app.route("/pacientes/create" , methods = [ "GET" , "POST"] )
+def create_paciente():
+    ####### mostrar el formulario : get ###########
+    if( request.method == "GET" ):
+        #el usuario ingreso con navegador con https://localhost.....
+        tipo_de_sangres = [
+            "A+",
+            "O+",
+            "A-",
+            "O-",
+            "AB+",
+            "AB-"
+        ]
+        return render_template("paciente_form.html",
+                            tipo_de_sangres = tipo_de_sangres)
+    
+
+#### Cuando el usuario presiona el boton de guardar#####
+#### los datos del formulario viajan al servidor
+    
+    elif(request.method == "POST"):
+        #cuando se presiona "guardar"
+        new_paciente = Paciente(nombre = request.form["nombre"],
+                            apellidos = request.form["apellidos"],
+                            tipo_identificacion = request.form["ti"],
+                            numero_identificacion = request.form["ni"],
+                            altura = request.form["al"],
+                            tipo_de_sangres = request.form["rh"]
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_paciente)
+        db.session.commit()
+        return "paciente registrado"
+    
+#crear ruta para crear nuevo consultorio
+@app.route("/consultorios/create" , methods = [ "GET" , "POST"] )
+def create_consultorio():
+    ####### mostrar el formulario : get ###########
+    if( request.method == "GET" ):
+        #el usuario ingreso con navegador con https://localhost.....
+        numero = [
+            "101",
+            "102",
+            "103",
+            "104",
+            "105",
+            "201",
+            "202",
+            "203",
+            "204",
+            "205"
+            
+        ]
+        return render_template("consultorio_form.html",
+                            numero = numero)
+    
+
+#### Cuando el usuario presiona el boton de guardar#####
+#### los datos del formulario viajan al servidor
+    
+    elif(request.method == "POST"):
+        #cuando se presiona "guardar"
+        new_consultorio = Consultorio( numero = request.form["nu"]
+                            )
+        #añadirlo a la sesion sqlalchemy
+        db.session.add(new_consultorio)
+        db.session.commit()
+        return "consultorio registrado"
+
+@app.route("/citas/create" , methods = [ "GET" , "POST"] )
+def create_cita():
+    if(request.method == "GET"):
+        pacientes = Paciente.query.all()
+        return render_template("cita_form.html" , pacientes=pacientes)
+    elif(request.method == "GET"):
+        medicos = Medico.query.all()
+        return render_template("cita_form.html" , medicos=medicos)
+    elif(request.method == "GET"):
+        consultorios = Consultorio.query.all()
+        return render_template("cita_form.html" , consultorios=consultorios)
+    elif(request.method == "POST"):
+        new_cita = Cita(fecha = request.form["fecha"],
+                        paciente = request.form["pac"],
+                        medico = request.form["med"],
+                        consultorio = request.form["con"],
+                        valor = request.form["val"]
+                        )
+        db.session.add(new_cita)
+        db.session.commit()
+        return "cita registrada"
+        
